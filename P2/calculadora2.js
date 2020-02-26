c311066193b4b44fc7acdce0b3cc94633b6962c6
ediez <ediez@casa>
2020-02-26 20:05:31 +0100

    calculadora casi terminada

--- P2/calculadora2.js
+++ P2/calculadora2.js
@@ -1,10 +1,37 @@
+
+// JavaScript by Eduardo Diez Blanco
+
 function cleanDisplay(calc){
   //Limpia el display
   calc.display.innerHTML = "0";
   //Aqui limpiaré los operadores
-  calc.op1 = null;
+  calc.op1 = null,
   calc.op2 = null;
   calc.operacion = null;
+  calc.resultado = null;
+}
+
+function operar(calc){
+  // Funcion para indicar operaciones se van a realizar
+  switch(calc.operacion){
+    case "+":
+      calc.resultado = Number(calc.op1) + Number(calc.op2);
+      calc.display.innerHTML = calc.resultado;
+    break;
+    case "-":
+        calc.resultado = Number(calc.op1) - Number(calc.op2);
+        calc.display.innerHTML = calc.resultado;
+      }
+    break;
+    case "x":
+      calc.resultado = Number(calc.op1) * Number(calc.op2);
+      calc.display.innerHTML = calc.resultado;
+    break;
+    case "/":
+      calc.resultado = Number(calc.op1) / Number(calc.op2);
+      calc.display.innerHTML = calc.resultado;
+    break;
+  }
 }
 function accionBotones(value, calc){
   // Defino lo que va a pasar una vez se clicken los botones
@@ -20,9 +47,21 @@ function accionBotones(value, calc){
         alert("Primero introduce un numero");
         cleanDisplay(calc);
       } else {
+        calc.operacion = value;
+        console.log(calc.operacion);
         calc.op1 = calc.display.innerHTML.split(value)[0];
         calc.display.innerHTML += value;
       }
+      break;
+      case "=":
+        if(calc.display.innerHTML == 0){
+          alert("Primero introduce un numero");
+          cleanDisplay(calc);
+        } else {
+          calc.op2= calc.display.innerHTML.split(value)[0].split(calc.op1)[1].replace(calc.operacion, "");
+          console.log(calc.operacion);
+          operar(calc);
+        }
       break;
     default:
         if(calc.display.innerHTML == "0"){
@@ -48,6 +87,7 @@ function clickButtons(calc){
       op1: null,
       op2: null,
       operacion: null,
+      resultado: null,
     }
       clickButtons(calc);
   }
