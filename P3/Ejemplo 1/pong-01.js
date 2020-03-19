4ceef4695b89f64c16dbe35f947285fe239e1bb1
ediez <ediez@casa>
2020-03-19 16:06:40 +0100

    Primer ejemplo terminado

---
+++ P3/Ejemplo 1/pong-01.js
@@ -0,0 +1,9 @@
+
+console.log("Ejecutando JS...");
+
+// Obteniendo el objeto canvas de DOM
+const canvas = document.getElementById("canvas");
+
+// Ahora se van a mostrar las dimensiones fijadas en el fichero
+// Anchura y altura
+console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
