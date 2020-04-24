5638d23eecd8a9234b9245345a95f1a1185fef67
ediez <ediez@casa>
2020-04-24 16:50:09 +0200

    sigo probando cosas

--- P4/video.js
+++ P4/video.js
@@ -12,7 +12,7 @@ var boton1 = document.getElementById("boton1");
 var boton2 = document.getElementById("boton2");
 var boton3 = document.getElementById("boton3");
 var auto = document.getElementById("auto");
-var num_vid = 1;
+
 // funcion para obtener los parametros del video
 function video_Parameters (video, width, height) {
   video.width = width;
@@ -20,12 +20,13 @@ function video_Parameters (video, width, height) {
   if(video == video1){
     video.poster = "barras.png";
   } else if (video == video2){
-    video.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4" + "#t=0,3";
+    video.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
   } else if (video == video3){
-    video.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4" + "#t=0,3";
+    video.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4";
   } else {
-    video.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4" + "#t=0,3";
+    video.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4";
   }
+
 }
 
 // Funcion que definira lo que haran los botones
@@ -38,23 +39,11 @@ function click_Botton(boton, video1, video2){
 }
 
 function automatico(video1,video2,video3,video4){
-  if(num_vid == 1){
     video1.src = video2.src;
     video1.currentTime = video2.currentTime;
     video1.play();
-    num_vid = 2;
-  } else if (num_vid == 2){
-    video1.src = video3.src;
-    video1.currentTime = video3.currentTime;
-    video1.play();
-    num_vid = 3;
-  } else{
-    video1.src = video4.src;
-    video1.currentTime = video4.currentTime;
-    video1.play();
-    num_vid = 1;
   }
-}
+
 // Obteniendo los parametros del video
 video_Parameters(video1, 600, 300);
 video_Parameters(video2, 200, 100);
@@ -62,7 +51,7 @@ video_Parameters(video3, 200, 100);
 video_Parameters(video4, 200, 100);
 
 auto.onclick = () => {
-  automatico(video1,video2,video3,video4)
+  automatico(video1,video2,video3,video4);
   window.setInterval('automatico(video1,video2,video3,video4)',2000);
 };
 /* // LLamando a la funcion de click_Botton
