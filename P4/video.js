ab89546a3e51e28223826293970cbd639f8f73cb
ediez <ediez@casa>
2020-04-25 01:11:28 +0200

    sigo probando cosas, mañana sustituire los botones por input

--- P4/video.js
+++ P4/video.js
@@ -13,7 +13,9 @@ var boton2 = document.getElementById("boton2");
 var boton3 = document.getElementById("boton3");
 var auto = document.getElementById("auto");
 var normal = document.getElementById("normal");
+var stop = document.getElementById("stop");
 var  inicio = false;
+var num_vid = 1;
 // funcion para obtener los parametros del video
 function video_Parameters (video, width, height) {
   video.width = width;
@@ -27,47 +29,60 @@ function video_Parameters (video, width, height) {
   } else {
     video.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4";
   }
-
 }
 
 // Funcion que definira lo que haran los botones
 function click_Botton(boton, video1, video2){
   boton.onclick = () => {
-      video1.src = video2.src;
-      video1.currentTime = video2.currentTime;
-      video1.play();
+      if(inicio == true){
+        video1.src = video2.src;
+        video1.currentTime = video2.currentTime;
+        video1.play();
+      }
   };
 }
 
-function automatico(video1,video2,video3,video4){
-    video1.src = video2.src;
-    video1.currentTime = video2.currentTime;
-    video1.play();
-  }
-
-// Obteniendo los parametros del video
 video_Parameters(video1, 600, 300);
 video_Parameters(video2, 200, 100);
 video_Parameters(video3, 200, 100);
 video_Parameters(video4, 200, 100);
-/*
+
+function automatico(video1,video2,video3,video4){
+    if(num_vid == 1){
+      video1.src = video2.src;
+      video1.currentTime = video2.currentTime;
+      video1.play();
+      num_vid += 1;
+    } else if (num_vid == 2){
+      video1.src = video3.src;
+      video1.currentTime = video3.currentTime;
+      video1.play();
+      num_vid += 1;
+    } else {
+      video1.src = video4.src;
+      video1.currentTime = video4.currentTime;
+      video1.play();
+    }
+  }
+
 auto.onclick = () => {
-  video2.play();
-  video3.play();
-  video4.play();
+  inicio = false;
   automatico(video1,video2,video3,video4);
   window.setInterval('automatico(video1,video2,video3,video4)',2000);
 };
 normal.onclick = () => {
+  video1.play();
   video2.play();
   video3.play();
-  video4.play();
   inicio = true;
 };
-*/
-video2.play();
-video3.play();
-video4.play();
+stop.onclick = () => {
+  video2.pause();
+  video3.pause();
+  video4.pause();
+  video1.currentTime = 0;
+  video1.src = null;
+};
 
 click_Botton(boton1,video1,video2);
 click_Botton(boton2,video1,video3);
