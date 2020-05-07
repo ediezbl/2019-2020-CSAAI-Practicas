47e412bc013c61bcc697e0d94bd7320a24e85f6c
ediez <ediez@casa>
2020-05-07 22:49:42 +0200

    funcion deslizador (en ella generalizo los tres deslizadores)

--- P5/filtros/filtro.js
+++ P5/filtros/filtro.js
@@ -1,6 +1,7 @@
 
-console.log("Ejecutando JS....")
+console.log("Ejecutando JS....");
 
+// constantes
 const canvas = document.getElementById('canvas');
 const img = document.getElementById('imagesrc');
 const ctx = canvas.getContext('2d');
@@ -11,23 +12,11 @@ const range_value_Rojo = document.getElementById('range_value_Rojo');
 const range_value_Verde = document.getElementById("range_value_Verde");
 const range_value_Azul = document.getElementById("range_value_Azul");
 
-img.onload = function () {
-  canvas.width = img.width;
-  canvas.height = img.height;
-  ctx.drawImage(img, 0,0);
-
-  console.log("Imagen lista...");
-};
-
-deslizadorRojo.oninput = () => {
-  range_value_Rojo.innerHTML = deslizadorRojo.value;
-  ctx.drawImage(img, 0,0);
-  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  let data = imgData.data;
-  umbralRojo = deslizadorRojo.value
+// funciones
+function filtroRojo(data){
   for (let i = 0; i < data.length; i+=4) {
-    if (data[i] > umbralRojo){
-        data[i] = umbralRojo;
+    if (data[i] > umbral){
+        data[i] = umbral;
       }
     if(data[i + 1] > deslizadorVerde.value){
       data[i + 1] = deslizadorVerde.value;
@@ -36,38 +25,12 @@ deslizadorRojo.oninput = () => {
       data[i + 2] = deslizadorAzul.value;
     }
   }
-    ctx.putImageData(imgData, 0, 0);
-  };
+}
 
-deslizadorVerde.oninput = () => {
-  range_value_Verde.innerHTML = deslizadorVerde.value;
-  ctx.drawImage(img, 0,0);
-  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  let data = imgData.data;
-  umbralVerde = deslizadorVerde.value
+function filtroVerde(data){
   for (let i = 0; i < data.length; i+=4) {
-    if (data[i + 1] > umbralVerde){
-      data[i + 1] = umbralVerde;
-    }
-    if(data[i] > deslizadorRojo.value){
-      data[i] = deslizadorRojo.value;
-    }
-    if(data[i + 2] > deslizadorAzul.value){
-      data[i + 2] = deslizadorAzul.value;
-    }
-  }
-  ctx.putImageData(imgData, 0, 0);
-};
-
-deslizadorAzul.oninput = () => {
-  range_value_Azul.innerHTML = deslizadorAzul.value;
-  ctx.drawImage(img, 0,0);
-  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  let data = imgData.data;
-  umbralAzul = deslizadorAzul.value
-  for (let i = 0; i < data.length; i+=4) {
-    if(data[i + 2] > umbralAzul){
-      data[i + 2] = deslizadorAzul;
+    if(data[i + 2] > umbral){
+      data[i + 2] = umbral;
     }
     if(data[i] > deslizadorRojo.value){
       data[i] = deslizadorRojo.value;
@@ -76,7 +39,50 @@ deslizadorAzul.oninput = () => {
       data[i + 1] = deslizadorVerde.value;
     }
   }
-  ctx.putImageData(imgData, 0, 0);
+}
+
+function filtroAzul(data){
+  for (let i = 0; i < data.length; i+=4) {
+    if(data[i + 2] > umbral){
+      data[i + 2] = umbral;
+    }
+    if(data[i] > deslizadorRojo.value){
+      data[i] = deslizadorRojo.value;
+    }
+    if(data[i] > deslizadorVerde.value){
+      data[i + 1] = deslizadorVerde.value;
+    }
+  }
+}
+
+function deslizador(color, deslizador, range_value){
+  deslizador.oninput = () => {
+    range_value.innerHTML = deslizador.value;
+    ctx.drawImage(img, 0,0);
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imgData.data;
+    umbral = deslizador.value;
+    // aqui es donde le paso el color
+    if(color == "rojo"){
+      filtroRojo(data);
+    } else if (color == "verde"){
+      filtroVerde(data);
+    } else if(color == "azul"){
+      filtroAzul(data);
+    }
+      ctx.putImageData(imgData, 0, 0);
+    };
+}
+img.onload = function () {
+  canvas.width = img.width;
+  canvas.height = img.height;
+  ctx.drawImage(img, 0,0);
+
+  console.log("Imagen lista...");
 };
+
+deslizador("rojo", deslizadorRojo, range_value_Rojo);
+deslizador("verde", deslizadorVerde, range_value_Verde);
+deslizador("azul", deslizadorAzul, range_value_Azul);
 
 console.log("Fin...");
