71dc7087baeb272505c83fad42d659a328246a19
ediez <ediez@casa>
2020-05-07 23:36:46 +0200

    filtro de grises hecho

--- P5/filtros/filtro.js
+++ P5/filtros/filtro.js
@@ -8,9 +8,15 @@ const ctx = canvas.getContext('2d');
 const deslizadorRojo = document.getElementById('deslizadorRojo');
 const deslizadorVerde = document.getElementById("deslizadorVerde");
 const deslizadorAzul = document.getElementById("deslizadorAzul");
+const color = document.getElementById("colores");
+const gris = document.getElementById("grises");
 const range_value_Rojo = document.getElementById('range_value_Rojo');
 const range_value_Verde = document.getElementById("range_value_Verde");
 const range_value_Azul = document.getElementById("range_value_Azul");
+
+// variables
+var colores = false;
+var grises = false;
 
 // funciones
 function filtroRojo(data){
@@ -54,25 +60,40 @@ function filtroAzul(data){
     }
   }
 }
+function filtroGris(){
+  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  let data = imgData.data;
+  ctx.drawImage(img, 0,0);
+  for (let i = 0; i < data.length; i+=4) {
+    brillo = (3 * data[i] + 4 * data[i + 1] + data[i + 2])/8
+    data[i] = brillo;
+    data[i + 1] = brillo;
+    data[i + 2] = brillo;
+  }
+  ctx.putImageData(imgData, 0, 0);
+};
 
 function deslizador(color, deslizador, range_value){
-  deslizador.oninput = () => {
-    range_value.innerHTML = deslizador.value;
-    ctx.drawImage(img, 0,0);
-    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    let data = imgData.data;
-    umbral = deslizador.value;
-    // aqui es donde le paso el color
-    if(color == "rojo"){
-      filtroRojo(data);
-    } else if (color == "verde"){
-      filtroVerde(data);
-    } else if(color == "azul"){
-      filtroAzul(data);
-    }
-      ctx.putImageData(imgData, 0, 0);
-    };
+    deslizador.oninput = () => {
+      if(colores){
+        range_value.innerHTML = deslizador.value;
+        ctx.drawImage(img, 0,0);
+        let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        let data = imgData.data;
+        umbral = deslizador.value;
+        // aqui es donde le paso el color
+        if(color == "rojo"){
+          filtroRojo(data);
+        } else if (color == "verde"){
+          filtroVerde(data);
+        } else if(color == "azul"){
+          filtroAzul(data);
+        }
+          ctx.putImageData(imgData, 0, 0);
+        }
+      };
 }
+
 img.onload = function () {
   canvas.width = img.width;
   canvas.height = img.height;
@@ -81,8 +102,20 @@ img.onload = function () {
   console.log("Imagen lista...");
 };
 
-deslizador("rojo", deslizadorRojo, range_value_Rojo);
-deslizador("verde", deslizadorVerde, range_value_Verde);
-deslizador("azul", deslizadorAzul, range_value_Azul);
+color.onclick = () => {
+    colores = true;
+    ctx.drawImage(img, 0,0);
+    deslizador("rojo", deslizadorRojo, range_value_Rojo);
+    deslizador("verde", deslizadorVerde, range_value_Verde);
+    deslizador("azul", deslizadorAzul, range_value_Azul);
+  };
+
+gris.onclick = () => {
+  grises = true;
+  colores = false;
+  if(grises){
+    filtroGris();
+  }
+};
 
 console.log("Fin...");
